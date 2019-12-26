8cd46bed37699fdd3c8386c363521b3e99529ab5
crowz-fx <crowzfx@outlook.com>
2019-12-26 20:42:31 +0000

    finished off the fractal tree

--- fractal-trees/sketch.js
+++ fractal-trees/sketch.js
@@ -1,7 +1,45 @@
+var angle;
+let screenSizeMultiplier = 0.7;
+let colourMultiplier = 0.70;
+let strokeMultiplier = 0.67;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(displayWidth * screenSizeMultiplier, displayHeight * screenSizeMultiplier);
 }
 
 function draw() {
-    background(220);
+    angle = TWO_PI * 0.93;
+    stroke(255);
+    translate(width / 2, height);
+    drawTree(height / 3.5);
+}
+
+function drawTree(length) {
+    // intial tree body
+    strokeWeight(length / 25);
+    stroke(0, 2 * length * colourMultiplier, 0);
+    line(0, 0, 0, -length);
+    translate(0, -length);
+
+    if(length > 4) {
+        // right branch
+        push();
+        rotate(angle);
+        strokeWeight(length * strokeMultiplier);
+        stroke(0, 2 * length * colourMultiplier, 0);
+        drawTree(length * 0.67);
+        pop();
+
+        // dots on the ends of the branches
+        fill(0, 2 * length * colourMultiplier, 0);
+        ellipse(0, 0, length / 12, length / 12);
+        
+        // left branch
+        push();
+        rotate(-angle);
+        strokeWeight(length * strokeMultiplier);
+        stroke(0, 2 * length * colourMultiplier, 0);
+        drawTree(length * 0.67);
+        pop();
+    }
 }
